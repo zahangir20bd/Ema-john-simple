97baed00eebe78fbb86f476394652a3fd3bb557f
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-27 01:08:38 +0600

    Header and body container setup done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css"
+import logo from "../../images/Logo.svg"
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h3>Products Coming Here: {products.length}</h3>
+      </div>
+      <div className="cart-container">
+        <h4>Cart summary</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
