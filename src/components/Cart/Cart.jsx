b420baf8c62a0ee455e29c33ea390a93ef401a94
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-27 23:45:21 +0600

    cart price calculation and display okey

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import './Cart.css'
+import Product from "../Product/Product";
+import "./Cart.css";
 
 const Cart = (props) => {
-    const {cart} = props
+  //   console.log(props);
+  const { cart } = props;
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+  }
   return (
-    <div>
+    <div className="cart">
       <h4>Order summary</h4>
       <p>Selected Items: {cart.length}</p>
+      <p>Total Price: {total}</p>
+      <p>Total Shipping: </p>
+      <p>Tax: </p>
+      <h4>Grand Total: </h4>
     </div>
   );
 };
