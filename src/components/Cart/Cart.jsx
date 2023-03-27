bdceb1660e59b61d1f12b94904d994f73aa9bc60
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-03-27 23:30:20 +0600

    Cart Componets add

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Cart.css'
+
+const Cart = (props) => {
+    const {cart} = props
+  return (
+    <div>
+      <h4>Order summary</h4>
+      <p>Selected Items: {cart.length}</p>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
@@ -29,9 +30,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h4>Order summary</h4>
-        {/* <p>Hello</p> */}
-        <p>Selected Items: {cart.length}</p>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
